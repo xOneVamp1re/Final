bed808c605c8e5d8c9ee515d479f89e6e65f4a57
Alex Lyadnik <lyadnik47@gmail.com>
2024-11-08 20:17:53 +0300

    Updates

--- main.de7dcf2286d984623c63.js
+++ main.de7dcf2286d984623c63.js
@@ -46,16 +46,30 @@ document.addEventListener('DOMContentLoaded', function () {
     item.addEventListener('click', function (event) {
       event.preventDefault();
       const buttonId = item.getAttribute('data-show-more');
-      console.log(buttonId);
-      const Elem = document.querySelector('.swiper-wrapper[data-show-more="' + buttonId + '"]');
-      if (item.classList.contains('button-show-more--active')) {
-        Elem.classList.remove('services-slider-wrapper--active');
-        item.classList.remove('button-show-more--active');
-        item.querySelector('span').textContent = 'Показать все';
+      console.log('id: ' + buttonId);
+      if (buttonId === 'about-us') {
+        let elem = document.querySelector('.text-wrapper[data-show-more="' + buttonId + '"]');
+        if (window.innerWidth > 1119) return;
+        if (item.classList.contains('button-show-more--active')) {
+          elem.classList.remove('text-wrapper--active');
+          item.classList.remove('button-show-more--active');
+          item.querySelector('span').textContent = 'Читать далее';
+        } else {
+          elem.classList.add('text-wrapper--active');
+          item.classList.add('button-show-more--active');
+          item.querySelector('span').textContent = 'Скрыть';
+        }
       } else {
-        Elem.classList.add('services-slider-wrapper--active');
-        item.classList.add('button-show-more--active');
-        item.querySelector('span').textContent = 'Скрыть';
+        let elem = document.querySelector('.swiper-wrapper[data-show-more="' + buttonId + '"]');
+        if (item.classList.contains('button-show-more--active')) {
+          elem.classList.remove('services-slider-wrapper--active');
+          item.classList.remove('button-show-more--active');
+          item.querySelector('span').textContent = 'Читать далее';
+        } else {
+          elem.classList.add('services-slider-wrapper--active');
+          item.classList.add('button-show-more--active');
+          item.querySelector('span').textContent = 'Скрыть';
+        }
       }
     });
   });
@@ -10734,4 +10748,4 @@ __webpack_require__.r(__webpack_exports__);
 
 /******/ })()
 ;
-//# sourceMappingURL=main.08a260aa344d1b710ceb.js.map
+//# sourceMappingURL=main.de7dcf2286d984623c63.js.map
